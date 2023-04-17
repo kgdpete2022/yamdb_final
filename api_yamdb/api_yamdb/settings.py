60dfd6c1a4eab983009547b7c3c48e30a42830d0
Пётр Шопин <mail4dev2022@gmail.com>
2023-04-17 21:30:16 +0200

    Updated 11

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -4,7 +4,6 @@ from django.conf.global_settings import DATETIME_INPUT_FORMATS
 from dotenv import load_dotenv
 from pathlib import Path
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(os.path.join(BASE_DIR.parent, 'infra/.env'))
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret')
