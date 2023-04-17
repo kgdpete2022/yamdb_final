2fef8c9e7a7423189f9e7c911edca8b14bad78be
Пётр Шопин <mail4dev2022@gmail.com>
2023-04-18 00:27:32 +0200

    Update v17

--- api_yamdb/reviews/management/commands/import_csv_data.py
+++ api_yamdb/reviews/management/commands/import_csv_data.py
@@ -1,46 +1,41 @@
-from api_yamdb.settings import CSV_FILES_DIR
-from django.core.management import BaseCommand
-from django.db import IntegrityError
-from reviews.models import (
-    Category,
-    Comments,
-    Genre,
-    GenreTitle,
-    Review,
-    Title
-)
-from users.models import User
 import csv
 import os
 
+from django.core.management import BaseCommand
+from django.db import IntegrityError
+from reviews.models import Category, Comments, Genre, GenreTitle, Review, Title
+from users.models import User
+
+from api_yamdb.settings import CSV_FILES_DIR
+
 FILES_CLASSES = {
-    'category': Category,
-    'genre': Genre,
-    'titles': Title,
-    'genre_title': GenreTitle,
-    'users': User,
-    'review': Review,
-    'comments': Comments,
+    "category": Category,
+    "genre": Genre,
+    "titles": Title,
+    "genre_title": GenreTitle,
+    "users": User,
+    "review": Review,
+    "comments": Comments,
 }
 
 FIELDS = {
-    'category': ('category', Category),
-    'title_id': ('title', Title),
-    'genre_id': ('genre', Genre),
-    'author': ('author', User),
-    'review_id': ('review', Review),
+    "category": ("category", Category),
+    "title_id": ("title", Title),
+    "genre_id": ("genre", Genre),
+    "author": ("author", User),
+    "review_id": ("review", Review),
 }
 
 
 def open_csv_file(file_name):
     """Менеджер контекста для открытия csv-файлов."""
-    csv_file = file_name + '.csv'
+    csv_file = file_name + ".csv"
     csv_path = os.path.join(CSV_FILES_DIR, csv_file)
     try:
-        with (open(csv_path, encoding='utf-8')) as file:
+        with open(csv_path, encoding="utf-8") as file:
             return list(csv.reader(file))
     except FileNotFoundError:
-        print(f'Файл {csv_file} н найден.')
+        print(f"Файл {csv_file} н найден.")
 
 
 def change_foreign_values(data_csv):
@@ -49,15 +44,14 @@ def change_foreign_values(data_csv):
     for field_key, field_value in data_csv.items():
         if field_key in FIELDS.keys():
             field_key0 = FIELDS[field_key][0]
-            data_csv_copy[field_key0] = FIELDS[field_key][1].objects.get(
-                pk=field_value)
+            data_csv_copy[field_key0] = FIELDS[field_key][1].objects.get(pk=field_value)
     return data_csv_copy
 
 
 def load_csv(file_name, class_name):
     """Осуществляет загрузку csv-файлов."""
-    table_not_loaded = f'Таблица {class_name.__qualname__} не загружена.'
-    table_loaded = f'Таблица {class_name.__qualname__} загружена.'
+    table_not_loaded = f"Таблица {class_name.__qualname__} не загружена."
+    table_loaded = f"Таблица {class_name.__qualname__} загружена."
     data = open_csv_file(file_name)
     rows = data[1:]
     for row in rows:
@@ -67,8 +61,7 @@ def load_csv(file_name, class_name):
             table = class_name(**data_csv)
             table.save()
         except (ValueError, IntegrityError) as error:
-            print(f'Ошибка в загружаемых данных. {error}. '
-                  f'{table_not_loaded}')
+            print(f"Ошибка в загружаемых данных. {error}. " f"{table_not_loaded}")
             break
     print(table_loaded)
 
@@ -78,5 +71,5 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for key, value in FILES_CLASSES.items():
-            print(f'Загрузка таблицы {value.__qualname__}')
+            print(f"Загрузка таблицы {value.__qualname__}")
             load_csv(key, value)
